7bc9aeab24124648d2a324bb3282034f14a10418
Aryan Anubhav Singh <116826634+Aryn1204@users.noreply.github.com>
2023-02-10 23:43:39 +0530

    Create quick_sort.cpp

---
+++ Recursion/quick_sort.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+    void quickSort(int arr[], int low, int high){
+        if(low>=high) return;
+        int p=partition(arr,low,high);
+        quickSort(arr,low,p-1);
+        quickSort(arr,p+1,high);
+    }
+    public:
+    int partition (int arr[], int low, int high){
+      int pivot=arr[low];
+      int i=low,j=high;
+      while(i<j)
+      {
+          while(arr[i]<=pivot) i++;
+          while(arr[j]>pivot) j--;
+          if(i<j) swap(arr[i],arr[j]);
+      }
+       swap(arr[j],arr[low]);
+       return j; 
+    }
+};
